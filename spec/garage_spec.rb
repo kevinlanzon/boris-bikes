299b5dfaae62b95b45ca584715f0fd75874c3b7c
Kevin Lanzon <kevinlanzon@gmail.com>
2015-02-06 17:15:51 +0000

    Fixed bug

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -3,7 +3,7 @@ require 'garage'
 describe Garage do
 	let(:garage) {Garage.new}
 	let(:broken_bike) {double :bike, {broken?: true}}
-	let(:fixed_bike) {double :bike, {broken?: false}}
+	let(:fixed_bike) {double :bike, {fix!: true}}
 
 	it 'should be able receive broken bikes' do
 		expect(garage.broken_bikes.count).to eq(0)
